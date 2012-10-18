3f6ec0b15a7a25d80a47b74398107129504744a2
Thomas Robitaille <thomas.robitaille@gmail.com>
2012-10-18 20:52:32 +0200

    Convert to plain SVG

--- convert.sh
+++ convert.sh
@@ -6,6 +6,12 @@ inkscape astropy_logo_linkout.svg --export-png=astropy_logo_linkout.png
 inkscape astropy_logo_docs.svg --export-png=astropy_logo_docs.png
 inkscape astropy_logo_small.svg --export-png=astropy_logo_small.png
 
+# Convert form Inkscape SVG to Plain SVG (for web use)
+inkscape astropy_logo.svg --export-plain-svg=astropy_logo_plain.svg
+inkscape astropy_logo_linkout.svg --export-plain-svg=astropy_logo_linkout_plain.svg
+inkscape astropy_logo_docs.svg --export-plain-svg=astropy_logo_docs_plain.svg
+inkscape astropy_logo_small.svg --export-plain-svg=astropy_logo_small_plain.svg
+
 # Convert to PNGs required by Astropy website/docs
 convert -intent Saturation -colorspace sRGB -filter Lanczos -resize 1000x96 -trim astropy_logo.png astropy_banner_96.png
 convert -colorspace sRGB -filter Box -resize 1000x31 -trim astropy_logo_docs.png astropy_docs_32.png
